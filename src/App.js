6527b1578882bbf131ba5379ef72545d26c6eb2f
Janbolot Batyrov <janbolotbatyrovoff@gmail.com>
2021-10-28 15:01:56 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { Route } from "react-router";
+import { Header } from "./components";
+import { Home, Cart } from "./pages";
+
+function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/db.json")
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
+        <Route path="/cart" component={Cart} exact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
